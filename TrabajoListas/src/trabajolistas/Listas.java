64297d35b64eafe4b5f0b8ac544fb7a98a3e587e
CarlosOrtizContreras <carlosaoc2611@gmail.com>
2023-03-14 22:44:16 -0500

    Se realizaron algunos cambios en los metodos de la clase Listas, dado que de Listas Doblemente ligadas, se paso a Listas doblemente ligadas - circulares.

--- TrabajoListas/src/trabajolistas/Listas.java
+++ TrabajoListas/src/trabajolistas/Listas.java
@@ -4,9 +4,17 @@ import javax.swing.JOptionPane;
 
 public class Listas {
     Nodo Punta;
+    Nodo Final;
 
+    /**
+     * @param Listas Este constructor permite agregar valores iniciales a un objeto
+     *               creado con la clase lista
+     * @param Final  Este nodo en el constructor, por defecto sera "null"
+     * @param Punta  Este nodo en el constructor, por defecto sera "null"
+     */
     public Listas() {
         Punta = null;
+        Final = null;
     }
 
     /**
@@ -16,61 +24,152 @@ public class Listas {
      *                       tipo entero que se va a agregar a la lista.
      * @param posicion       Esta variable, permite ubicar el inicio de la lista, en
      *                       caso de que se mueva la punta, dicha lista no se pierda
+     * @param Final          Esta variable tipo Nodo permite ubicar el fin de la
+     *                       lista
+     * @param Punta          Esta variable tipo Nodo permite Ubicar el inicio de
+     *                       dicha lista
+     * @param nuevo          Esta variable tipo Nodo es el nuevo objeto creado que
+     *                       se va a agregar a la lista
      */
     public void InsertarInicio(int entrada) {
         Nodo posicion;
         Nodo nuevo = new Nodo(entrada);
         if (Punta == null) {
             Punta = nuevo;
+            Final = Punta;
+            Punta.setLigaDerecha(Punta);
+            Punta.setLigaIzquierda(Punta);
         } else {
             posicion = Punta;
             Punta = nuevo;
-            nuevo.setLigaDerecha(posicion);
-            posicion.setLigaIzquierda(nuevo);
+            Final.setLigaDerecha(Punta);
+            Punta.setLigaIzquierda(Final);
+            Punta.setLigaDerecha(posicion);
+            posicion.setLigaIzquierda(Punta);
+        }
+
+    }
+
+    /**
+     * @param InsertarFinal Esta funcion permite ingresar datos a la lista en la
+     *                      ultima posicion
+     * @param entrada       Es el atributo que va a tener la funcion como entrada de
+     *                      datos tipo enteros para agregar a la lista
+     * @param nuevo         Es el nuevo objeto o nodo que se crea para agregar a la
+     *                      lista
+     * @param posicion      Esta variable tipo nodo, permite recorrer la lista,
+     *                      hasta encontrar el ultimo nodo o la ultima posicion y
+     *                      alli insertar el valor
+     * @param Punta         Esta variable tipo nodo Indica el nodo de inicio de la
+     *                      lista
+     * @param Final         Esta VARIABLE TIPO nodo Indica el nodo final de la lista
+     */
+    public void InsertarFinal(int entrada) {
+        Nodo posicion;
+        Nodo nuevo = new Nodo(entrada);
+
+        if (Punta == null) {
+            Punta = nuevo;
+            Final = Punta;
+            Punta.setLigaDerecha(Punta);
+            Punta.setLigaIzquierda(Punta);
+        } else {
+            posicion = Punta;
+
+            while (posicion.getLigaDerecha() != Punta) {
+                posicion = posicion.getLigaDerecha();
+            }
+            Final = nuevo;
+            posicion.setLigaDerecha(Final);
+            Final.setLigaIzquierda(posicion);
+            Final.setLigaDerecha(Punta);
+            Punta.setLigaIzquierda(Final);
         }
     }
 
-    public void InsertarFinal(int entrada){
-        Nodo posicion;
-        Nodo nuevo = new Nodo (entrada);
+    public void InsertarOrdenadoAscendente(int entrada) {
 
-        if (Punta == null){
-            Punta = nuevo;
-        } else{
-            posicion = Punta;
-            while (posicion.getLigaDerecha()!= null){
-                posicion = posicion.getLigaDerecha();
-            }
-            posicion.setLigaDerecha(nuevo);
-            nuevo.setLigaIzquierda(posicion);
-        }
-    } 
+    }
 
-    public void OrdenarAscendente (Listas lis){
-        if (lis.Punta == null){
+    public void SumaListas() {
+
+    }
+
+    public void OrdenarAscendente() {
+        if (Punta == null) {
             JOptionPane.showMessageDialog(null, "La lista esta vacia");
-        }else{
+        } else {
             Nodo posicion1;
             Nodo posicion2;
             Nodo auxiliar;
+            Nodo auxiliar2;
+            Nodo auxiliar3;
+            Nodo auxiliar4;
             posicion1 = Punta;
-            while (posicion1.getLigaDerecha()!= null){
+            while (posicion1.getLigaDerecha() != null) {
                 posicion2 = posicion1.getLigaDerecha();
-                    while(posicion2.getLigaDerecha()!= null){
-                        if(posicion2.getDato()< posicion1.getDato()){
-                            
+                boolean condicion = true;
+                while (condicion) {
+                    if (posicion2.getDato() <= posicion1.getDato()) {
+                        auxiliar = posicion2.getLigaIzquierda();
+                        auxiliar2 = posicion2.getLigaDerecha();
+                        auxiliar.setLigaDerecha(posicion2.getLigaDerecha());
+                        auxiliar2.setLigaIzquierda(posicion2.getLigaIzquierda());
+                        if (Punta == posicion1) {
+                            auxiliar3 = posicion1;
+                            posicion1 = posicion2;
+                            posicion1.setLigaDerecha(auxiliar3);
+                            posicion1.setLigaIzquierda(null);
+                            auxiliar3.setLigaIzquierda(posicion1);
+                            Punta = posicion1;
+                            posicion2 = auxiliar;
+
+                            /*
+                             * Punta = posicion2;
+                             * posicion2.setLigaDerecha(posicion1);
+                             * posicion2.setLigaIzquierda(null);
+                             * posicion1.setLigaIzquierda(posicion2);
+                             */
+                        } else {
+                            auxiliar3 = posicion1;
+                            posicion1 = posicion2;
+                            posicion1.setLigaDerecha(auxiliar3);
+                            posicion1.setLigaIzquierda(auxiliar3.getLigaIzquierda());
+                            auxiliar.setLigaIzquierda(posicion1);
+                            auxiliar4 = posicion1.getLigaIzquierda();
+                            auxiliar4.setLigaDerecha(posicion1);
+                            posicion2 = auxiliar;
+
+                            /*
+                             * posicion2.setLigaIzquierda(posicion1.getLigaIzquierda());
+                             * posicion2.setLigaDerecha(posicion1);
+                             * posicion1.setLigaIzquierda(posicion2);
+                             */
                         }
                     }
+                    if (posicion2.getLigaDerecha() == null) {
+                        condicion = false;
+                    } else {
+                        posicion2 = posicion2.getLigaDerecha();
+
+                    }
+                }
+                posicion1 = posicion1.getLigaDerecha();
+
             }
         }
     }
 
+    
     public void MostrarLista() {
         if (Punta != null) {
             String salida = " ";
-            for (Nodo p = Punta; p != null; p.getLigaDerecha()) {
-                salida = salida + " | " + p.getDato() + " | ";
+            Nodo posicion = Punta;
+            do{
+                salida = salida + " | " + posicion.getDato() + " | ";
+                posicion = posicion.getLigaDerecha();
             }
+            while(posicion != Punta);
             JOptionPane.showMessageDialog(null, salida);
         } else {
             JOptionPane.showMessageDialog(null, "La lista esta vacia");
--- TrabajoListas/src/trabajolistas/TrabajoListas.java
+++ TrabajoListas/src/trabajolistas/TrabajoListas.java
@@ -1,11 +1,43 @@
 
 package trabajolistas;
 
+import javax.swing.JOptionPane;
+
 public class TrabajoListas {
 
- 
     public static void main(String[] args) {
-        
+        boolean condicion = true;
+        int opcion;
+        JOptionPane.showMessageDialog(null,
+                "Vienvenido a nuestro menu de Listas, En el cual podras realizar lo siguiente: ");
+        do {
+            opcion = Integer.parseInt((JOptionPane.showInputDialog("          MENU      \n" + "1. Insertar datos a la lista\n" + "2. Ordenar los datos de la lista\n" + "3. Buscar datos \n" + "4. Mostrar Lista\n" + "5. Operaciones con dos listas\n" + "9. Salir del menu")));
+            switch (opcion) {
+                case 1:
+                    boolean condicion2 = true;
+                    do {
+                        int opcion2 = Integer.parseInt((JOptionPane.showInputDialog("        SUBMENU       \n" + "1. Insertar al inicio de la lista\n" + "2. Insertar al Final de la lista\n" + "3. Insertar Ordenado Ascendente\n" + "4. Insertar Ordenado Descendente\n" + "9. Salir del Submenu")));
+                        switch (opcion2) {
+                            case 1:
+                            case 9:
+                                condicion2 = false;
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "Ingrese una opcion valida");
+                                break;
+                        }
+                        break;
+                    } while (condicion2);
+                    break;
+                case 9:
+                    JOptionPane.showMessageDialog(null, "Muchas gracias por utilizar nuestro programa");
+                    condicion = false;
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Ingrese una opcion valida");
+                    break;
+            }
+        } while (condicion);
     }
-    
+
 }
